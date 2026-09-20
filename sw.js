1cbb4743967f2750ac4cf685f36a41f4b2168367
Kass1313 <inutilisables@gmail.com>
2026-09-20 20:33:54 +0200

    Cache advanced minigame build

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='raphy-v10-20260920';
-const ASSETS=['./','./index.html','./styles.css','./enhancements.css','./app.js','./app-v2a.js','./app-v2b.js','./app-v2c.js','./app-v2d.js','./manifest.json','./icon.svg'];
+const CACHE='raphy-v11-20260920';
+const ASSETS=['./','./index.html','./styles.css','./enhancements.css','./enhancements-v2.css','./app.js','./app-v2a.js','./app-v2b.js','./app-v2c.js','./app-v2d.js','./app-v2e.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match('./index.html'))))});
