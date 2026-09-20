3a743141e418fbf40c38b3dceb21e7d710627eaa
Kass1313 <inutilisables@gmail.com>
2026-09-20 20:14:13 +0200

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='raphy-v7-20260920';
+const ASSETS=['./','./index.html','./styles.css','./app-1.js','./app-2.js','./app-3.js','./app-4.js','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match('./index.html'))))});
