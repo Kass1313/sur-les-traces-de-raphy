c07f4d0495dc8f07e172a811ccb3373a60772565
Kass1313 <inutilisables@gmail.com>
2026-09-20 20:51:54 +0200

    Bump PWA cache for cinematic act polish

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='raphy-v16-20260920';
+const CACHE='raphy-v17-20260920';
 const ASSETS=['./','./index.html','./styles.bundle.css','./app.bundle.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
